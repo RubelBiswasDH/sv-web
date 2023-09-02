6babf4397cf0c1ea22befc2cde8d09a7dba1cf76
RubelBiswasDH <RubelBiswasDH@gmail.com>
2023-09-02 19:01:13 +0600

    Added map page and openlayer map

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { onMount } from 'svelte';
+    import './style.css';
+    import Map from 'ol/Map.js';
+    import OSM from 'ol/source/OSM.js';
+    import TileLayer from 'ol/layer/Tile.js';
+    import View from 'ol/View.js';
+
+    const renderMap = () => {
+        const map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 4,
+            }),
+        });
+    }
+
+    onMount(() => {
+        renderMap()
+    })
+  
+</script>
+
+<div>
+    <div id='map'></div>
+</div>
